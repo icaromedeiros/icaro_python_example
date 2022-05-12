0012ecf195c0fe4e33f948706997062b72996bdc
icaro <icaro.medeiros@gmail.com>
2022-05-12 11:27:35 +0200

    hello world and import example

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Hello World
+'''
+from names import medeiros
+
+# By far the ugliest line you'll read on Python
+if __name__ == "__main__":
+    # What do you think it's going to come out of this command?
+    print(medeiros)
